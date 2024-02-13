6fd0d46cc19759b5dd13dc90115e2c88b01735dd
Muhammed Yasin Özdemir <94251353+MuhammedYasinOzdemirDev@users.noreply.github.com>
2024-02-13 21:31:59 +0300

    Add files via upload

---
+++ Properties/Properties/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+   class Product
+    {
+        private double _fiyat;
+        public double fiyat//degisken get set gibi kullanilmaya yarar
+        {
+            get
+            {
+                    return _fiyat;//fiyat dondururu ama get gerek yok
+            }
+            set
+            {
+                if(value>0)//girilen değr value ile temsil edilir
+                    _fiyat = value;//kosula gore yazılır
+                else
+                    _fiyat = 0;//sart saglnmassa otomatik atılır
+
+            }
+        }
+        private string _name;
+        public string name { get { return _name; } set
+            {
+                if (!string.IsNullOrEmpty(value))//bos veya null degilse 
+                    _name = value;//atilir
+                else
+                    Console.WriteLine("Deger giriniz...");//burda hata fırlatılabilir
+
+            } }
+        public bool durum { get; }//set olmadığı için erişilebilir fakat değiştirilemez
+        public Product()
+        {
+            durum = true;
+        }
+    }
+}
---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,11 @@
+﻿using Properties;
+Product product = new Product();
+product.fiyat = -500;//set metodu çalışır 0 atanır
+Console.WriteLine(product.fiyat);//get çalışır 0 yazar
+product.fiyat = 500;//set metodu çalışır 500 atanır
+Console.WriteLine(product.fiyat);//get çalışır 500 yazar
+product.name = "";//hata mesajı verir set calisir
+product.name = "affa";//;deger atanir
+Console.WriteLine(product.durum);//durumu ogrenir
+//product.durum = false; bu kod hatalı set kodu yok baska metodla erişimeli
+
