e9ba10cbee2023d6a96332391d2f10f59df13cd6
Muhammed Yasin Özdemir <94251353+MuhammedYasinOzdemirDev@users.noreply.github.com>
2024-02-13 21:23:55 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp1
+{
+
+     class Product
+    {
+        [Key]//primary key özelliğ verir
+        public int Id { get; set; }
+        [MaxLength(100)]//max 100 karekter oolmasını sağlar
+        [Required]//zorunlu alan olmasını sağlar
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+    class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+    }
+    class ShopContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+       
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=YASIN\SQLEXPRESS;Initial Catalog=BookStoreDb;");
+        }
+    }
+}
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp1;
+using MyS
+Console.WriteLine();
+ShopContext db = new ShopContext();
+
+    var p = new Product();
+    p.Name = "s";
+    p.Price= 100;
+    db.Products.Add(p);
+   db.SaveChanges();
